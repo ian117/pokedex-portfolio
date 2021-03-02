f9b8c31236c41bd3e32c5b3e5269f4984285e343
ian117 <spideramerica71@gmail.com>
2021-03-02 11:08:16 -0600

    Just pagination and protected page

--- src/components/PokemonCardsandAxios.js
+++ src/components/PokemonCardsandAxios.js
@@ -1,25 +1,33 @@
 import React, { useState,useEffect} from 'react'
 import PokemonCardGroup from "./PokemonCardGroup";
+import styled from "styled-components"
+
 
 
 function PokemonCardsandAxios({lista}) {
 
     const [ component, setComponent] = useState([])
+    const [ post, setPost] = useState([])
+    const [ loading, setLoading] = useState(false)
+    const [ postPerPage, setPostPerPage] = useState(4)
 
     const makeList = (arreglo) => {
-        const allItems = arreglo.map((item) => {
-            if (item === null) {
-                return
-            }
-            return <PokemonCardGroup key={item.name} data={item}/>
-        })
+        if (arreglo.length === 0){
+            const nothing = <h2>No Luck</h2>
+            return setComponent(nothing)
+        }
+        const allItems = <DisplayGrid>
+                        {arreglo.map((item) => {
+                        return <PokemonCardGroup key={item.name} data={item}/>
+                            })}
+                    </DisplayGrid>
         setComponent(allItems)
     }
 
     useEffect( async () => {      
         try {
             const auxArr = []
-            lista.splice(0, 20).map((data) => {
+            lista.map((data) => {
                 auxArr.push(fetch(data.pokemon.url)
                 .then(raw => raw.json())
                 .then(result => result))}
@@ -45,3 +53,14 @@ function PokemonCardsandAxios({lista}) {
 }
 
 export default PokemonCardsandAxios
+
+
+const DisplayGrid = styled.div`
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+
+    div {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+`
